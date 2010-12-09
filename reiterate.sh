39fc6e16afaacc84ee4e25de7a255e2e0a091318
Andrey Brindeyev <abrindeyev@griddynamics.com>
2010-12-08 16:10:24 -0800

    Changed snap id again

--- reiterate.sh
+++ reiterate.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 VM="RHEL6_node"
-SNAPID="a09ecda3-9efd-4138-a97b-9bfbadb4bf8f"
+SNAPID="97055158-7ae3-4394-afd0-93a35789d80a" # run 'ssh mac prlctl snapshot-list RHEL6_node' to get it
 
 ssh mac prlctl stop $VM --kill
 ssh mac prlctl snapshot-switch $VM -i $SNAPID
 ssh mac prlctl start $VM
+echo "Sleeping a bit to allow VM to boot"
 sleep 20
 ssh n /root/install.sh
