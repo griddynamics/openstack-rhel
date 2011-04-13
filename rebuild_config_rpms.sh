401bde4ebefb640bf8501d5a451bd73e782d0696
Andrey Brindeyev <abrindeyev@griddynamics.com>
2011-04-13 16:39:59 +0400

    Added rebuild script for Nova config rpms
    Signed-off-by: Andrey Brindeyev <abrindeyev@griddynamics.com>

---
+++ rebuild_config_rpms.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ ! -d ".git" ]; then
+	echo "Need to run from Git repo openstack-nova-rhel6 !"
+	exit -1
+fi
+sandbox='/home/build/rpmbuild/RPMS/noarch'
+branch=$(git branch|grep '*'|cut -f2 -d' ')
+repo="/home/build/repo/$branch/nova/"
+host=`hostname -s`
+if [[ "$host" != "je-cssn" ]]; then
+	echo "That script should be run only on Jenkins host!"
+fi
+
+rpmbuild -ba openstack-nova-cc-config.spec
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+rpmbuild -ba openstack-nova-compute-config.spec
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+./sign_rpm.sh $sandbox/openstack-nova-cc-config-*.rpm
+./sign_rpm.sh $sandbox/openstack-nova-compute-config-*.rpm
+
+rm -f $repo/openstack-nova-cc-config*.rpm
+rm -f $repo/openstack-nova-compute-config*.rpm
+
+mv $sandbox/openstack-nova-cc-config-*.rpm $sandbox/openstack-nova-compute-config-*.rpm $repo
+
